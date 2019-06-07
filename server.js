de37db41f2c56810011172b4e319c2b8306d09c4
dallcharm <dallcharm@gmail.com>
2019-06-06 20:41:32 -0500

    adding handlers

--- server.js
+++ server.js
@@ -9,3 +9,24 @@ res.send('Hello World!')
 app.listen(port, function () { 
 console.log(`Example app listening on port ${port}!`)
 })
+
+
+// GET method route
+app.get('/', function (req, res) {
+    res.send('Contestando una peticion por GET');
+  });
+  
+  // POST method route
+  app.post('/', function (req, res) {
+    res.send('Contestando una peticion por POST');
+  });
+
+  // PUT method route
+app.put('/', function (req, res) {
+    res.send('Contestando una peticion por PUT');
+  });
+  
+  // DELETE method route
+  app.delete('/', function (req, res) {
+    res.send('Contestando una peticion por DELETE');
+  });
